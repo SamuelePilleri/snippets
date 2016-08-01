05d4b7529a045cc34e4806d92fd0664fd87e91f3
Samuele Pilleri <pillerisamuele@gmail.com>
2016-08-01 19:35:58 +0200

    Improved argshift with compile time string switch-cases.

--- cppargs_shift/README.md
+++ cppargs_shift/README.md
@@ -1,1 +1,6 @@
 Parsing arguments like in bash, using only lambdas.
+
+Requires C++11. Compiles with:
+```g++ args.cpp --std=c++11 -Wall -pedantic```
+and
+```clang++ args.cpp --std=c++11 -Wall -pedantic```
--- cppargs_shift/args.cpp
+++ cppargs_shift/args.cpp
@@ -8,19 +8,34 @@
 
 using namespace std;
 
-string join(const vector<string>& vec, const char* delim)
+inline string join(const vector<string>& vec, const char* delim)
 {
     ostringstream res;
     copy(vec.begin(), vec.end(), ostream_iterator<string>(res, delim));
     return res.str();
 }
 
-void parse(int argc, const char *argv[], function<void( string&& param, function<string()> next )>&& callback )
+/**
+ * Compile time string hashing.
+ *
+ * If using inside a switch-case statement, just don't care about collisions
+ * since, if any, they are reported as an error by the compiler.
+ *
+ * See: https://stackoverflow.com/questions/2111667/compile-time-string-hashing
+ */
+constexpr unsigned int chash(char const *str)
+{
+	return *str ?
+		(static_cast<unsigned int>(*str) + 33 * chash(str + 1)) :
+		5381;
+}
+
+void parse(int argc, char const *argv[], function<void( string&& param, function<string()> next )>&& callback )
 {
 	int i = 1;
 	
 	auto n = [&i, &argc, &argv]() -> string {
-		return (i < argc) ? string(argv[i++]) : "";
+		return (i < argc) ? argv[i++] : "";
 	};
 	
 	while(i < argc)
@@ -29,31 +44,89 @@ void parse(int argc, const char *argv[], function<void( string&& param, function
 	}
 }
 
-int main(int argc, const char *argv[])
+void parse(int argc, char const *argv[], function<void( string&& param, function<string()> next, function<string()> peek )>&& callback )
+{
+	int i = 1;
+	
+	auto n = [&]() -> string {
+		return (i < argc) ? argv[i++] : "";
+	};
+
+	auto p = [&]() -> string {
+		return (i < argc) ? argv[i] : "";
+	};
+	
+	while(i < argc)
+	{
+		callback(string(argv[i++]), n, p);
+	}
+}
+
+int main(int argc, char const *argv[])
 {
 	vector<string> ax;
 	parse(argc, argv, [&ax](string&& param, function<string()> next){
-		/*switch(param)
+		switch(chash(param.c_str()))
 		{
-			case "-h":
-			case "--help":
+			case chash("-e"):
+			case chash("--experimental"):
+				cout << "Experimental: " << next() << endl;
+				break;
+
+			case chash("-f"):
+			case chash("--flag"):
+				cout << "Flag: true" << endl;
+				break;
+
+			case chash("-h"):
+			case chash("--help"):
+				cout << "Shows this help" << endl;
 				break;
 			
-			case "-v":
-			case "--version":
+			case chash("-v"):
+			case chash("--version"):
+				cout << "Version: 2.0" << endl;
 				break;
 			
 			default:
+				ax.push_back(param);
 				break;
-		}*/
-		if(param == "-e" || param == "--experimental")
-			cerr << "-e: " << next() << endl;
-		else if(param == "-f" || param == "--flag")
-			cerr << "-f: true" << endl;
-		else
-			ax.push_back(param);
-		
+		}
 	});
-	cerr << join(ax, " ") << endl;
+	cout << "Args: " << join(ax, " ") << endl;
+
+	// -------------------------------------------------------------------------
+	printf("--------------------------------------------------------------------------------\n");
+
+	vector<string> eax;
+	parse(argc, argv, [&](string&& param, function<string()> next, function<string()> peek){
+		switch(chash(param.c_str()))
+		{
+			case chash("-e"):
+			case chash("--experimental"):
+				cout << "Experimental: " << peek() << endl;
+				break;
+
+			case chash("-f"):
+			case chash("--flag"):
+				cout << "Flag: true" << endl;
+				break;
+
+			case chash("-h"):
+			case chash("--help"):
+				cout << "Shows this help" << endl;
+				break;
+			
+			case chash("-v"):
+			case chash("--version"):
+				cout << "Version: 2.0" << endl;
+				break;
+			
+			default:
+				eax.push_back(param);
+				break;
+		}
+	});
+	cout << "Args: " << join(eax, " ") << endl;
 	return 0;
 }
