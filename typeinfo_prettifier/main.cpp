b0fd0bcf44beb53a2c2e00b3b0ef47e7eec6b3ea
Samuele Pilleri <pillerisamuele@gmail.com>
2016-09-25 16:08:48 +0200

    Bugfix and minor.

--- typeinfo_prettifier/main.cpp
+++ typeinfo_prettifier/main.cpp
@@ -2,18 +2,17 @@
 
 using namespace std;
 
-#ifdef __has_include
-#	if __has_include(<cxxabi.h>)
+#if defined(__has_include) && __has_include(<cxxabi.h>)
 
-#	include <cstdlib>
-#	include <cstring>
-#	include <cxxabi.h>
+#  include <cstdlib>
+#  include <cstring>
+#  include <cxxabi.h>
 
 /**
- *	Prettify a type_info.name() string, based on Eli Bendersky's great article:
- *	http://eli.thegreenplace.net/2015/programmatic-access-to-the-call-stack-in-c/
+ *  Prettify a type_info.name() string, based on Eli Bendersky's great article:
+ *  http://eli.thegreenplace.net/2015/programmatic-access-to-the-call-stack-in-c/
  */
-char* foo(const char* name)
+char* prettify_name(const char* name)
 {
 	// This makes the function not thread safe, but I don't care as of now.
 	// In a multithreaded environment, just return `demangled`, but remember to
@@ -31,23 +30,15 @@ char* foo(const char* name)
 	return ret;
 }
 
-#	endif /* __has_include(<cxxabi.h>) */
-#else /* __has_include */
+#else
+#  define prettify_name(x) x
+#  pragma message "Warning: No type_info::name prettify support"
+#endif
 
 /*
- *	If you don't have needed headers and such, just return the input.
- */
-inline char* foo(const char* name)
-{
-	return name;
-}
-
-#endif /* __has_include */
-
-/*
- *	The header <typeinfo> must be included before using typeid (if the header is
- *	not included, every use of the keyword typeid makes the program ill-formed.)
- *	(http://en.cppreference.com/w/cpp/language/typeid) 
+ *  The header <typeinfo> must be included before using typeid (if the header is
+ *  not included, every use of the keyword typeid makes the program ill-formed.)
+ *  (http://en.cppreference.com/w/cpp/language/typeid) 
  */
 #include <typeinfo>
 
@@ -62,7 +53,7 @@ namespace bar
 int main(int argc, const char* argv[])
 {
 	bar::quxx a;
-	cout << "Original: " << typeid(a).name() << endl;
-	cout << "fooed   : " << foo(typeid(a).name()) << endl;
+	cout << "Original  : " << typeid(a).name() << endl;
+	cout << "Prettified: " << prettify_name(typeid(a).name()) << endl;
 	return 0;
 }
